69972449860b763f77eb83aeb248dc63d4cb1589
Seoyeong <herz8081@gmail.com>
2022-05-18 13:38:16 +0900

    chapter8 finished

--- Project/BankingSystemVer.cpp
+++ Project/BankingSystemVer.cpp
@@ -6,6 +6,12 @@ const int NAME_LEN=20;
 
 enum {MAKE=1, DEPOSIT, WITHDRAW, INQUIRE, EXIT};
 
+namespace creditGrade{
+    enum {classA=7,classB=4,classC=2};
+}
+
+enum {NORMAL=1,HIGHCREDIT=2};
+
 /*
 Account class
 entity class
@@ -33,7 +39,7 @@ public:
     {
         return accID;
     }
-    void DepositMoney(int money)
+    virtual void DepositMoney(int money)
     {
         balance+=money;
         cout<<"입금완료"<<endl<<endl;
@@ -55,11 +61,42 @@ public:
         cout<<"이 름: "<<cusName<<endl;
         cout<<"잔 액: "<<balance<<endl<<endl;
     }
-    ~Account()
+    virtual ~Account()
     {
         delete []cusName;
     }
 
+};
+
+
+class NormalAccount : public Account
+{
+private:
+    const int ratio;
+public:
+    NormalAccount(int accID, int balance, char* cusName, int ratio) 
+    : Account(accID, balance, cusName), ratio(ratio)
+    {}
+    void DepositMoney(int money)
+    {
+        Account::DepositMoney(money);
+        Account::DepositMoney((int)(money*ratio/100));
+    }
+};
+
+class HighCreditAccount : public NormalAccount
+{
+private:
+    int credit;
+public:
+    HighCreditAccount(int accID, int balance, char* cusName, int ratio, int credit)
+    : NormalAccount(accID, balance, cusName, ratio), credit(credit)
+    {}
+    void DepositMoney(int money)
+    {
+        NormalAccount::DepositMoney(money);
+        Account::DepositMoney(money*credit/100);
+    }
 };
 
 /*
@@ -85,19 +122,66 @@ public:
         cout<<"5. 프로그램 종료"<<endl;
     }
 
-    void MakeAccount(void)
+    void MakeNormalAccount(void)
     {
         int id;
         char name[NAME_LEN];
         int balance;
+        int interest;
 
-        cout<<"[계좌개설]"<<endl;
+        cout<<"[보통예금계좌 개설]"<<endl;
         cout<<"계좌ID: "; cin>>id;
         cout<<"이 름: "; cin>>name;
         cout<<"입금액: "; cin>>balance;
+        cout<<"이자율: "; cin>>interest;
+        cout<<endl;
+        accPnt[accNum++]=new NormalAccount(id,balance,name,interest);
+    }
+
+    void MakeHighCreditAccount(void)
+    {
+        int id;
+        char name[NAME_LEN];
+        int balance;
+        int interest;
+        int credit;
+
+        cout<<"[신용신뢰계좌 개설]"<<endl;
+        cout<<"계좌ID: "; cin>>id;
+        cout<<"이 름: "; cin>>name;
+        cout<<"입금액: "; cin>>balance;
+        cout<<"이자율: "; cin>>interest;
+        cout<<"신용등급(1toA, 2toB, 3toC): "; cin>>credit;
         cout<<endl;
 
-        accPnt[accNum++]=new Account(id, balance, name);
+        switch(credit)
+        {
+            case 1: accPnt[accNum++]=new HighCreditAccount(id,balance,name,interest,creditGrade::classA); break;
+            case 2: accPnt[accNum++]=new HighCreditAccount(id,balance,name,interest,creditGrade::classB); break;
+            case 3: accPnt[accNum++]=new HighCreditAccount(id,balance,name,interest,creditGrade::classC); break;
+            default: cout<<"Wrong credit input"<<endl; return;
+        }
+    }
+
+    void MakeAccount(void)
+    {
+        int account;
+
+        cout<<"[계좌종류선택]"<<endl;
+        cout<<"1.보통예금계좌 2.신용신뢰계좌"<<endl<<"선택: "; cin>>account;
+        
+        switch(account)
+        {
+            case NORMAL:
+                MakeNormalAccount();
+                break;
+            case HIGHCREDIT:
+                MakeHighCreditAccount();
+                break;
+            default:
+                cout<<"Wrong Account"<<endl;
+                break;
+        }
     }
 
     void DepositMoney(void)
@@ -186,5 +270,6 @@ int main(void)
             cout<<"Illegal selection.."<<endl;
         }
     }
+    return 0;
 }
 
